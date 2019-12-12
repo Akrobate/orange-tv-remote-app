341556086bc0cc1087cc0c52643e86db7f1ccd78
Akrobate <fedorov.artiom@gmail.com>
2019-12-12 23:26:24 +0100

    First layouts test, icons uses full size

--- lib/main.dart
+++ lib/main.dart
@@ -24,6 +24,53 @@ class Home extends StatelessWidget {
         backgroundColor: Colors.grey[850],
         elevation: 0.0,
       ),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: ConstrainedBox(
+                    constraints: BoxConstraints(minWidth: double.infinity, minHeight: double.infinity),
+                    child: FittedBox(
+                      fit: BoxFit.fill,
+                      child: IconButton(
+                        onPressed: () {},
+                        icon: FittedBox(
+                          fit: BoxFit.fill,
+                          child: Icon(Icons.home),
+                        ),
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: IconButton(
+                    onPressed: () {},
+                    icon: Icon(Icons.home),
+                    color: Colors.white,
+                  ),
+                ),
+                Expanded(
+                  child: IconButton(
+                    onPressed: () {},
+                    icon: Icon(Icons.arrow_left),
+                    color: Colors.white,
+                  ),
+                ),
+                Expanded(
+                  child: IconButton(
+                    onPressed: () {},
+                    icon: Icon(Icons.arrow_left),
+                    color: Colors.white,
+                  ),
+                )
+              ],
+            ),
+          )
+        ],
+      )
     );
   }
 }
