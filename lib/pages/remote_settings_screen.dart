fd3b89a20094c5f1be3ce214c6a783988bd5899a
Akrobate <fedorov.artiom@gmail.com>
2019-12-19 22:30:23 +0100

    Scaffolding the parameters screen

--- lib/pages/remote_settings_screen.dart
+++ lib/pages/remote_settings_screen.dart
@@ -8,6 +8,10 @@ class RemoteSettingsScreen extends StatefulWidget {
 class _RemoteSettingsScreenState extends State<RemoteSettingsScreen> {
   @override
   Widget build(BuildContext context) {
+
+    bool useClearTheme = false;
+    bool useSimpleRemoteController = false;
+
     return Scaffold(
       backgroundColor: Colors.black,
       appBar: AppBar(
@@ -16,6 +20,57 @@ class _RemoteSettingsScreenState extends State<RemoteSettingsScreen> {
         backgroundColor: Colors.grey[850],
         elevation: 0.0,
       ),
+      body: Center(
+        child:  Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+                'Utiliser la télécommande simplifiée',
+                style: TextStyle(
+                  fontSize: 24.0,
+                  color: Colors.white,
+                )
+            ),
+            Transform.scale(
+              scale: 2.0,
+              child: Switch(
+                onChanged: (value) {
+                  setState(() {
+                    useSimpleRemoteController = value;
+                  });
+                },
+                value: useSimpleRemoteController,
+                activeTrackColor: Colors.lightGreenAccent,
+                activeColor: Colors.green,
+                inactiveTrackColor: Colors.green[900],
+              ),
+            ),
+            SizedBox(height: 40.0),
+            Text(
+                'Télécommande sur fond clair',
+                style: TextStyle(
+                  fontSize: 24.0,
+                  color: Colors.white,
+                )
+            ),
+            Transform.scale(
+              scale: 2.0,
+              child: Switch(
+                onChanged: (value) {
+                  setState(() {
+                    useClearTheme = value;
+                  });
+                },
+                value: useClearTheme,
+                activeTrackColor: Colors.lightGreenAccent,
+                activeColor: Colors.green,
+                inactiveTrackColor: Colors.green[900],
+              ),
+            ),
+
+          ]
+        )
+      )
     );
   }
 }
