96721eb6aba955321210305f40e06996410c0c86
Akrobate <fedorov.artiom@gmail.com>
2019-12-29 21:39:52 +0100

    Fixed default portrait mode, added param to manage debug mode

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:orange_tv_remote_app/pages/remote_controller_screen.dart';
 import 'package:orange_tv_remote_app/pages/remote_settings_screen.dart';
+import 'package:flutter/services.dart';
 
-void main() => runApp(MaterialApp(
-    initialRoute: '/',
-    routes: {
-      '/': (context) => RemoteControllerScreen(),
-      '/controller_screen': (context) => RemoteControllerScreen(),
-      '/settings_screen': (context) => RemoteSettingsScreen(),
-    }
-));
+void main() {
 
+  const bool debug_mode = false;
+
+  SystemChrome
+    .setPreferredOrientations([DeviceOrientation.portraitUp])
+    .then((_) {
+      runApp(MaterialApp(
+          initialRoute: '/',
+          debugShowCheckedModeBanner: debug_mode,
+          routes: {
+            '/': (context) => RemoteControllerScreen(),
+            '/controller_screen': (context) => RemoteControllerScreen(),
+            '/settings_screen': (context) => RemoteSettingsScreen(),
+          }
+      ));
+    });
+}
